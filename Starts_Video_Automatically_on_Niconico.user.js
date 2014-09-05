91202d88196d336c19a9357b3103f0463e889163
nodaguti <nodaguti@gmail.com>
2014-09-05 11:08:05 +0900

    Partially supporting Niconico Ginza
    This script still cannot make a video start when you go on to a next
    video or go back to the previous video.

--- Starts_Video_Automatically_on_Niconico.user.js
+++ Starts_Video_Automatically_on_Niconico.user.js
@@ -6,12 +6,10 @@
 // @license        MIT License
 // @namespace      http://nodaguti.usamimi.info/
 // @include        http://www.nicovideo.jp/watch/*
-// @updateURL      https://userscripts.org/scripts/source/35194.meta.js
+// @updateURL      https://raw.githubusercontent.com/nodaguti/Greasemonkey/master/Starts_Video_Automatically_on_Niconico.user.js
 // ==/UserScript==
 
-(function(){
-
-//======自動再生======
+(function(_window){
 
 //----config----
 //自動再生を試行する間隔 (ms)
@@ -19,35 +17,17 @@ var INTERVAL = 250;
 //----/config----
 
 
-//Harajuku
-if(document.getElementById("video_controls")){
+function playVideo(){
+    try{
+        if(!_window.WatchApp.namespace.model.player.NicoPlayerConnector.isPrepared)
+            return void(setTimeout(arguments.callee, INTERVAL));
 
-	location.href = "javascript:(function(){\
-\
-		var flvplayer = document.getElementById('flvplayer');\
-		var interval = " + INTERVAL + ";\
-\
-		if(!flvplayer || typeof flvplayer.ext_getStatus != 'function')\
-			return void(setTimeout(arguments.callee, interval));\
-\
-		flvplayer.ext_play(1);\
-		if(flvplayer.ext_getStatus().indexOf('playing') < 0) return void(setTimeout(arguments.callee, interval));\
-	})();";
-
-//Q Watch
-}else if(document.getElementById('nicoplayerContainerInner')){
-
-	var w = unsafeWindow;
-
-	try{
-		if(!w.WatchApp.namespace.model.player.NicoPlayerConnector.isPrepared)
-			return void(setTimeout(arguments.callee, INTERVAL));
-
-		w.WatchApp.namespace.model.player.NicoPlayerConnector.playVideo();
-	}catch(e){
-		setTimeout(arguments.callee, INTERVAL);
-	}
-
+        _window.WatchApp.namespace.model.player.NicoPlayerConnector.playVideo();
+    }catch(e){
+        setTimeout(arguments.callee, INTERVAL);
+    }
 }
 
-})();
+playVideo();
+
+})(unsafeWindow);
